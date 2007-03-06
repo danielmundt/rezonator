ca2142aadff88eda3214c364b6173fdc7fd69322
daniel.schubert <daniel.schubert@8cf6efb1-f42a-0410-81ad-11548b01ce4d>
2007-03-06 22:53:44 +0000

    Changed notch struct into a class.

--- source/notch.h
+++ source/notch.h
@@ -1,19 +1,19 @@
-/*  Rezonator VST Plugin (based on the Resonate sources by Don Cross)
-    Copyright (C) 2007 Daniel Schubert
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+// Rezonator VST Plugin (based on the Resonate sources by Don Cross)
+// Copyright (C) 2007 Daniel Schubert
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #ifndef NOTCH_H_
 #define NOTCH_H_
@@ -21,8 +21,9 @@
 namespace Rezonator
 {
 
-struct Notch
+class Notch
 {
+private:
 	double coeff;		//!< attenuation coefficient for demod
 	double accum;		//!< demod accumulators (one per channel)
 	double remod;		//!< remod accumulators (one per channel)
@@ -33,6 +34,10 @@ struct Notch
 	double ca;			//!< current cos(n*theta)
 	double sa;			//!< current sin(n*theta)
 
+public:
+	Notch();
+	virtual ~Notch();
+	
 	void init( double theta, double _coeff );
 };
 
