0f112d174a523ba73319bd54987aa41e1022e777
daniel.schubert <daniel.schubert@8cf6efb1-f42a-0410-81ad-11548b01ce4d>
2007-03-06 22:47:29 +0000

    Added notch skeleton.

--- trunk/source/notch.cpp
+++ trunk/source/notch.cpp
@@ -1,14 +1,39 @@
+/*  Rezonator VST Plugin (based on the Resonate sources by Don Cross)
+    Copyright (C) 2007 Daniel Schubert
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
+#include <math.h>
+
 #include "Notch.h"
 
 namespace Rezonator
 {
 
-Notch::Notch()
+void Notch::init( double theta, double _coeff )
 {
-}
+	coeff = _coeff;
+	alpha = sin( 0.5 * theta );
+	alpha = 2.0 * alpha * alpha;
+	beta  = sin( theta );
+	
+	sa = 0.0;
+	ca = 1.0;
 
-Notch::~Notch()
-{
+	accum = 0.0;
+	remod = 0.0;
 }
 
 }
