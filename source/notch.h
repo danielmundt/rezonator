30cb91f2ba58d3691ec019fb7d20867852121dfd
daniel.schubert <daniel.schubert@8cf6efb1-f42a-0410-81ad-11548b01ce4d>
2007-03-06 22:47:29 +0000

    Added notch skeleton.

--- source/notch.h
+++ source/notch.h
@@ -1,14 +1,39 @@
+/*  Rezonator VST Plugin (based on the Resonate sources by Don Cross)
+    Copyright (C) 2007 Daniel Schubert
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
 #ifndef NOTCH_H_
 #define NOTCH_H_
 
 namespace Rezonator
 {
 
-class Notch
+struct Notch
 {
-public:
-	Notch();
-	virtual ~Notch();
+	double coeff;		//!< attenuation coefficient for demod
+	double accum;		//!< demod accumulators (one per channel)
+	double remod;		//!< remod accumulators (one per channel)
+
+	double alpha;		//!< recurrence relation constant
+	double beta;		//!< recurrence relation constant
+
+	double ca;			//!< current cos(n*theta)
+	double sa;			//!< current sin(n*theta)
+
+	void init( double theta, double _coeff );
 };
 
 }
