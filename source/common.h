b14039b53e3b75a2a2a820b9d0336f9ff32408f0
daniel.schubert <daniel.schubert@8cf6efb1-f42a-0410-81ad-11548b01ce4d>
2007-03-13 22:03:21 +0000

    Made header C-proof (for what is it good for?)

--- source/common.h
+++ source/common.h
@@ -19,7 +19,15 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define VST_MAKE_ID( a, b, c, d ) \
   ( ( long )( a ) << 24L | ( long )( b ) << 16L | ( c ) << 8 | ( d ) )
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /*COMMON_H_*/
