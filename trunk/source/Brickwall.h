cf228a1f67e74042675d569febfaeb5f0c396e94
daniel.schubert <daniel.schubert@8cf6efb1-f42a-0410-81ad-11548b01ce4d>
2007-03-13 20:42:11 +0000

    Added IEffect interface declaration.

--- trunk/source/Brickwall.h
+++ trunk/source/Brickwall.h
@@ -1,14 +1,18 @@
 #ifndef BRICKWALL_H_
 #define BRICKWALL_H_
 
+#include "IEffect.h"
+
 namespace Rezonator
 {
 
-class Brickwall
+class Brickwall : IEffect
 {
 public:
 	Brickwall();
 	virtual ~Brickwall();
+	
+	void process( float** inputs, float** outputs, VstInt32 sampleFrames );
 };
 
 }
