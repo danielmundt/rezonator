e93daabf4cfc352252307a85bbc7a913693a2e08
daniel.schubert <daniel.schubert@8cf6efb1-f42a-0410-81ad-11548b01ce4d>
2007-07-03 15:02:52 +0000

    Added eclipse 4.0 project file

--- source/Rezonator.h
+++ source/Rezonator.h
@@ -15,7 +15,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
- 
+
 #ifndef REZONATOR_H_
 #define REZONATOR_H_
 
